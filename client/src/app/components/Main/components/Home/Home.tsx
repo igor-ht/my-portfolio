9cd7ab5360f32288a0ff212e705840d8de36269c
igor-ht <i_ht93@hotmail.com>
2023-06-02 05:40:08 +0300

    home page in proccess

--- client/src/app/components/Main/components/Home/Home.tsx
+++ client/src/app/components/Main/components/Home/Home.tsx
@@ -6,32 +6,7 @@ export default function Home() {
 	return (
 		<div id="home">
 			<div className="intro">
-				<div className="intro-avatar">
-					<Image
-						id="pic1"
-						src={'/mr.svg'}
-						alt="Mr."
-						height={450}
-						width={450}
-						priority
-					/>
-					<Image
-						id="pic2"
-						src={'/mr-burger.svg'}
-						alt="Mr. Burger"
-						height={450}
-						width={450}
-						priority
-					/>
-					<Image
-						id="pic3"
-						src={'/mr-burger-developer.svg'}
-						alt="Mr. Burger Developer"
-						height={450}
-						width={450}
-						priority
-					/>
-				</div>
+				<div className="intro-avatar" />
 				<div className={`intro-text ${roboto_mono.className}`}>
 					<p id="phrase1">Hi there!</p>
 					<p id="phrase2">My name is Igor Hamburger.</p>
