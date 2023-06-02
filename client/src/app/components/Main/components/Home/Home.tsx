1904be7f48fc94fabb7686ad2d02675240e893ec
igor-ht <i_ht93@hotmail.com>
2023-06-02 06:23:20 +0300

    home page in proccess

--- client/src/app/components/Main/components/Home/Home.tsx
+++ client/src/app/components/Main/components/Home/Home.tsx
@@ -8,9 +8,19 @@ export default function Home() {
 			<div className="intro">
 				<div className="intro-avatar" />
 				<div className={`intro-text ${roboto_mono.className}`}>
-					<p id="phrase1">Hi there!</p>
-					<p id="phrase2">My name is Igor Hamburger.</p>
-					<p id="phrase3">And I am a Junior Full Stack Web Developer.</p>
+					<span>
+						<p>Hi there!</p>
+					</span>
+					<span>
+						<p>
+							My name is <mark>Igor Hamburger</mark>.
+						</p>
+					</span>
+					<span>
+						<p>
+							And I am a <mark>Junior Full Stack Web Developer</mark>.
+						</p>
+					</span>
 				</div>
 			</div>
 		</div>
