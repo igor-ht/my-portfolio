cf22f1a5ee37529146674d37f9ce2713b5ff618c
igor-ht <i_ht93@hotmail.com>
2023-06-02 03:18:58 +0300

    home page in proccess

--- client/src/app/components/Main/components/Home/Home.tsx
+++ client/src/app/components/Main/components/Home/Home.tsx
@@ -1,14 +1,41 @@
 import './style.scss';
-import { red_hat } from '@/libs/fonts';
+import Image from 'next/image';
+import { roboto_mono } from '@/libs/fonts';
 
 export default function Home() {
 	return (
 		<div id="home">
 			<div className="intro">
-				<div className="intro-background" />
-				<div className={`intro-text ${red_hat.className}`}>
-					<p>Hello There! I am Igor, a Junior FullStack Web Developer.
-          </p>
+				<div className="intro-avatar">
+					<Image
+						id="pic1"
+						src={'/mr.svg'}
+						alt="Mr."
+						height={450}
+						width={450}
+						priority
+					/>
+					<Image
+						id="pic2"
+						src={'/mr-burger.svg'}
+						alt="Mr. Burger"
+						height={450}
+						width={450}
+						priority
+					/>
+					<Image
+						id="pic3"
+						src={'/mr-burger-developer.svg'}
+						alt="Mr. Burger Developer"
+						height={450}
+						width={450}
+						priority
+					/>
+				</div>
+				<div className={`intro-text ${roboto_mono.className}`}>
+					<p id="phrase1">Hi there!</p>
+					<p id="phrase2">My name is Igor Hamburger.</p>
+					<p id="phrase3">And I am a Junior Full Stack Web Developer.</p>
 				</div>
 			</div>
 		</div>
--- client/src/libs/fonts.ts
+++ client/src/libs/fonts.ts
@@ -1,4 +1,4 @@
-import { Indie_Flower, Red_Hat_Display } from 'next/font/google';
+import { Indie_Flower, Roboto_Mono } from 'next/font/google';
 
 export const indie_flower = Indie_Flower({ subsets: ['latin'], weight: '400', display: 'swap' });
-export const red_hat = Red_Hat_Display({ subsets: ['latin'], weight: '400', display: 'swap' });
+export const roboto_mono = Roboto_Mono({ subsets: ['latin'], weight: '400', display: 'swap' });
