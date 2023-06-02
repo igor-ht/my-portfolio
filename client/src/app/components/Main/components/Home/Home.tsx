ffe46ef28fd7c38c649288bf43ad9dc189030ace
igor-ht <i_ht93@hotmail.com>
2023-06-02 23:56:20 +0300

    intro video created

--- client/src/app/components/Main/components/Home/Home.tsx
+++ client/src/app/components/Main/components/Home/Home.tsx
@@ -6,22 +6,13 @@ export default function Home() {
 	return (
 		<div id="home">
 			<div className="intro">
-				<div className="intro-avatar" />
-				<div className={`intro-text ${roboto_mono.className}`}>
-					<span>
-						<p>Hi there!</p>
-					</span>
-					<span>
-						<p>
-							My name is <mark>Igor Hamburger</mark>.
-						</p>
-					</span>
-					<span>
-						<p>
-							And I am a <mark>Junior Full Stack Web Developer</mark>.
-						</p>
-					</span>
-				</div>
+				<Image
+					src={'/mr.burger-video.gif'}
+					alt="intro"
+					height={500}
+					width={500}
+					unoptimized={true}
+				/>
 			</div>
 		</div>
 	);
