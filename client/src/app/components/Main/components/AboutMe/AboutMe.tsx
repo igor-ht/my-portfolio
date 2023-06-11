f383174da2856af42f203081d02e3b5912ae983a
igor-ht <i_ht93@hotmail.com>
2023-06-11 19:29:07 +0300

    home page gif responsible dimensions in progress

--- client/src/app/components/Main/components/AboutMe/AboutMe.tsx
+++ client/src/app/components/Main/components/AboutMe/AboutMe.tsx
@@ -1,35 +1,31 @@
+import { cormorant_garamond } from '@/libs/fonts';
 import './style.scss';
 
 export default function AboutMe() {
 	return (
 		<div id="about-me">
-			<div>
-				<p>
-					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque ratione provident eum odit laboriosam repudiandae dolore sapiente.
-					Necessitatibus perferendis nam natus aspernatur ut. Adipisci rerum eum, distinctio ducimus deserunt provident!
+			<h1 className="title">About Me:</h1>
+
+			<div className="resume">
+				<p className={`${cormorant_garamond.className}`}>
+					A passionate Junior FullStack Developer with main focus in <mark> Web Development</mark>.
+					<br />
+					With a year of hands-on experience in creating websites and web applications, my goal is to leverage my skills and knowledge to
+					contribute to innovative projects and continuously grow as a developer.
+					<br />
+					<br />
+					During my journey as a Junior FullStack Web Developer, I have had the opportunity to work on various projects that involved
+					implementing responsive designs, optimizing website performance, integrating APIs, authentication and authorization, Database .
+					This experience has equipped me with a solid foundation in web development principles and best practices.
+					<br />
+					As a lifelong learner, I am committed to staying up-to-date with the latest technologies and industry trends. I aim to continually
+					enhance my skills by exploring new frameworks and tools that empower me to create cutting-edge web solutions. My goal is to
+					contribute to a collaborative team where I can apply my knowledge and grow professionally.
 				</p>
-			</div>
-			<div>
-				<p>
-					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo animi corrupti magni consectetur, doloribus non assumenda molestiae
-					maiores aut quo quas itaque, praesentium impedit libero quos? Ex nam laborum error. Nobis porro recusandae repudiandae nostrum
-					quos culpa pariatur vero atque beatae, voluptatum iure minima obcaecati, maiores corporis molestias fugiat exercitationem eum,
-					sint facilis voluptas architecto expedita. Perferendis ratione ad sit. Officiis dicta alias in optio quibusdam voluptates
-					deserunt, veniam debitis dolore quis esse exercitationem possimus odit modi aspernatur est fugit dolores hic incidunt nobis porro
-					laboriosam? Quo distinctio in blanditiis! Ducimus perspiciatis pariatur sapiente eveniet vitae ut dolore doloremque esse sequi
-					repudiandae error omnis, dicta expedita sunt porro iusto cupiditate sint reiciendis odit, odio neque? Minus necessitatibus dolores
-					et repellat! Id ad molestias ratione incidunt eaque laudantium reiciendis debitis quae aliquid odio in, enim, minus adipisci nulla
-					voluptates eum repudiandae numquam nemo, libero et quo tempore veniam facilis excepturi? Perferendis? Distinctio rerum facere
-					voluptatem nemo incidunt hic numquam, laboriosam eos nam itaque mollitia, consectetur aspernatur qui dicta quos, excepturi
-					deleniti. Voluptatibus vel dolorem, laudantium quo consequuntur itaque nisi commodi distinctio? Nostrum corporis sequi optio
-					consequuntur impedit possimus earum magni velit illo deleniti rerum harum commodi voluptas numquam perferendis, nisi soluta ab
-					incidunt voluptatibus at accusamus, laudantium saepe. Optio, porro! Porro. Iure nobis dolorum veniam delectus! Itaque iste
-					repellat repudiandae quae nam provident eum placeat ipsa temporibus? Omnis cum, consequuntur sint ipsam ratione dolores iusto
-					quasi quos a sequi iste eaque? Obcaecati dolores maxime dolor omnis quisquam nemo accusamus rem pariatur facilis aspernatur in
-					minus odit provident dolorem nobis, quod deleniti ipsa? Explicabo aperiam provident aliquid nobis temporibus labore, excepturi
-					iusto! Recusandae blanditiis, tempore maiores ratione laborum quidem? Ipsa delectus nemo, itaque accusamus, pariatur velit
-					possimus, quibusdam eius adipisci debitis ex vitae dolorem tempore non! Earum nesciunt porro architecto iusto culpa.
-				</p>
+
+				<section className="skills">
+					<h1>Technologies:</h1>
+				</section>
 			</div>
 		</div>
 	);
--- client/src/libs/fonts.ts
+++ client/src/libs/fonts.ts
@@ -1,3 +1,13 @@
-import { Indie_Flower } from 'next/font/google';
+import { Indie_Flower, Cormorant_Garamond } from 'next/font/google';
 
-export const indie_flower = Indie_Flower({ subsets: ['latin'], weight: '400', display: 'swap' });
+export const indie_flower = Indie_Flower({
+	subsets: ['latin'],
+	weight: '400',
+	display: 'swap',
+});
+
+export const cormorant_garamond = Cormorant_Garamond({
+	subsets: ['latin'],
+	weight: '400',
+	display: 'swap',
+});
