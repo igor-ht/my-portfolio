5455853377de5a3a4228ba7feeb4ead58013e07b
igor-ht <i_ht93@hotmail.com>
2023-06-11 21:15:05 +0300

    about-me section in proccess

--- client/src/app/components/Main/components/AboutMe/AboutMe.tsx
+++ client/src/app/components/Main/components/AboutMe/AboutMe.tsx
@@ -1,5 +1,6 @@
-import { cormorant_garamond } from '@/libs/fonts';
 import './style.scss';
+import { cormorant_garamond } from '@/libs/fonts';
+import Image from 'next/image';
 
 export default function AboutMe() {
 	return (
@@ -8,15 +9,15 @@ export default function AboutMe() {
 
 			<div className="resume">
 				<p className={`${cormorant_garamond.className}`}>
-					A passionate Junior FullStack Developer with main focus in <mark> Web Development</mark>.
+					A passionate Junior FullStack Developer with main focus in Web Development.
 					<br />
 					With a year of hands-on experience in creating websites and web applications, my goal is to leverage my skills and knowledge to
 					contribute to innovative projects and continuously grow as a developer.
 					<br />
 					<br />
 					During my journey as a Junior FullStack Web Developer, I have had the opportunity to work on various projects that involved
-					implementing responsive designs, optimizing website performance, integrating APIs, authentication and authorization, Database .
-					This experience has equipped me with a solid foundation in web development principles and best practices.
+					implementing responsive designs, optimizing website performance, integrating APIs and more. This experience has equipped me with a
+					solid foundation in web development principles and best practices.
 					<br />
 					As a lifelong learner, I am committed to staying up-to-date with the latest technologies and industry trends. I aim to continually
 					enhance my skills by exploring new frameworks and tools that empower me to create cutting-edge web solutions. My goal is to
@@ -25,6 +26,92 @@ export default function AboutMe() {
 
 				<section className="skills">
 					<h1>Technologies:</h1>
+					<section className="icons">
+						<Image
+							src="/techs/python.svg"
+							alt="Python Programming Language"
+							height="48"
+							width="48"
+						/>
+						<Image
+							src="/techs/html.svg"
+							alt="HTML"
+							height="48"
+							width="48"
+						/>
+						<Image
+							src="/techs/css.svg"
+							alt="CSS"
+							height="48"
+							width="48"
+						/>
+						<Image
+							src="/techs/scss.svg"
+							alt="SCSS"
+							height="48"
+							width="48"
+						/>
+						<Image
+							src="/techs/nodejs.svg"
+							alt="Node.js"
+							height="48"
+							width="48"
+						/>
+						<Image
+							src="/techs/javascript.svg"
+							alt="JavaScript Programming Language"
+							height="48"
+							width="48"
+						/>
+						<Image
+							src="/techs/typescript.svg"
+							alt="TypeScript"
+							height="48"
+							width="48"
+						/>
+						<Image
+							src="/techs/react-js.svg"
+							alt="React.js"
+							height="48"
+							width="48"
+						/>
+						<Image
+							src="/techs/next-js.svg"
+							alt="Next.js"
+							height="48"
+							width="48"
+						/>
+						<Image
+							src="/techs/express-js.svg"
+							alt="Express.js"
+							height="48"
+							width="48"
+						/>
+						<Image
+							src="/techs/jwt.svg"
+							alt="JSON Web Token"
+							height="48"
+							width="48"
+						/>
+						<Image
+							src="/techs/postgresql.svg"
+							alt="PostgreSQL"
+							height="48"
+							width="48"
+						/>
+						<Image
+							src="/techs/mongodb.svg"
+							alt="MongoDB"
+							height="48"
+							width="48"
+						/>
+						<Image
+							src="/techs/prisma.svg"
+							alt="Prisma ORM"
+							height="48"
+							width="48"
+						/>
+					</section>
 				</section>
 			</div>
 		</div>
