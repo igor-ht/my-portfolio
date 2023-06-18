cec36cca4d569fbe589c0cb6042fd8e665227292
igor-ht <i_ht93@hotmail.com>
2023-06-18 19:35:05 +0300

    background images updated

--- client/src/app/components/Main/components/Projects/Project/Project.tsx
+++ client/src/app/components/Main/components/Projects/Project/Project.tsx
@@ -14,7 +14,7 @@ export default function Project({ props }: { props: ProjectPropsType }) {
 			ref={props.ref || null}
 			onAnimationEnd={(event) => (event.currentTarget.style.animation = '')}>
 			<div className="project-display">
-				<h1 className={`${karla.className} title`}>{props.name}</h1>
+				<h1 className={`${karla.className} project-title`}>{props.name}</h1>
 				<div className="content">
 					<section className="video-stack"></section>
 					<section className="description-links"></section>
