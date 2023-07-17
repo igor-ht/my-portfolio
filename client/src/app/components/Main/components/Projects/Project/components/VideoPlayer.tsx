6eb645de1a8ba8fae9223f861f347af027ff7855
igor-ht <i_ht93@hotmail.com>
2023-07-17 14:22:35 +0300

    projects videoplayer improved

--- client/src/app/components/Main/components/Projects/Project/components/VideoPlayer.tsx
+++ client/src/app/components/Main/components/Projects/Project/components/VideoPlayer.tsx
@@ -1,52 +1,19 @@
-'use client';
-
 import dynamic from 'next/dynamic';
 import Image from 'next/image';
-import { useReducer, CSSProperties, useRef } from 'react';
 
 const ReactPlayer = dynamic(() => import('react-player/lazy'), { ssr: false });
 
-type stateType = Pick<CSSProperties, ('height' & 'width') | 'position' | 'top' | 'left'> & {
-	height: string;
-	width: string;
-	position?: string;
-	top?: string;
-	left?: string;
-};
-
-type actionType = {
-	type: boolean;
-};
-
-const initialStyle: stateType = {
-	height: '70%',
-	width: '95%',
-	position: 'unset',
-	top: 'none',
-	left: 'none',
-};
-
-const dispatchStyle = (state: stateType, action: actionType): stateType => {
-	switch (action.type) {
-		case true:
-			return initialStyle;
-			//return { height: '100%', width: '100%', position: 'absolute', left: '0', top: '0' };
-		case false:
-			return initialStyle;
-		default:
-			return state;
-	}
-};
-
 export default function VideoPlayer(props: { videoUrl: string; picUrl: string }) {
-	const [currentStyle, dispatchCurrentStyle] = useReducer(dispatchStyle, initialStyle);
-	const videoWrapper = useRef<HTMLDivElement | null>(null);
-
 	return (
 		<div
 			className="videoplayer"
-			style={currentStyle}
-			ref={videoWrapper}>
+			style={{
+				height: '70%',
+				width: '95%',
+				position: 'unset',
+				top: 'none',
+				left: 'none',
+			}}>
 			<ReactPlayer
 				className="react-player"
 				url={props.videoUrl}
@@ -63,8 +30,6 @@ export default function VideoPlayer(props: { videoUrl: string; picUrl: string })
 						alt={'thumbnail'}
 					/>
 				}
-				onPause={() => dispatchCurrentStyle({ type: false })}
-				onStart={() => dispatchCurrentStyle({ type: true })}
 			/>
 		</div>
 	);
