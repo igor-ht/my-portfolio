1c045f97665b757fe30257f6efead592b3c9c959
igor-ht <i_ht93@hotmail.com>
2023-08-16 00:23:58 +0300

    getData moved to server component Main

--- client/src/app/components/Main/Main.tsx
+++ client/src/app/components/Main/Main.tsx
@@ -3,9 +3,12 @@ import Home from './components/Home/Home';
 import AboutMe from './components/AboutMe/AboutMe';
 import Projects from './components/Projects/Projects';
 import ContactMe from './components/ContactMe/ContactMe';
-import { Projects as Project, Skills } from '@prisma/client';
+import { use } from 'react';
+import getData from '@/libs/getData';
 
-export default function Main({ data }: { data: { skills: Skills[]; projects: Project[] } }) {
+export default function Main() {
+	const data = use(getData());
+	
 	return (
 		<main id="main">
 			<Home />
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,5 @@
-import getData from '@/libs/getData';
 import Main from './components/Main/Main';
 
-export default async function Page() {
-	const data = await getData();
-	return <Main data={data} />;
+export default function Page() {
+	return <Main />;
 }
