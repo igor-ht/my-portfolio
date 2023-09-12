d9efa5a70f167f5f55e3afe29a5f1fcf14a66ae5
igor-ht <i_ht93@hotmail.com>
2023-09-12 11:34:23 +0300

    codepen icon + link added to contac-me section

--- client/src/app/components/Main/components/ContactMe/ContactMe.tsx
+++ client/src/app/components/Main/components/ContactMe/ContactMe.tsx
@@ -18,6 +18,7 @@ export default function ContactMe() {
 				<Link
 					href="mailto: idht07@gmail.com"
 					rel="nofollow"
+					title="Email me"
 					target="_blank">
 					<Image
 						src={'/email-icon.svg'}
@@ -29,6 +30,7 @@ export default function ContactMe() {
 				</Link>
 				<Link
 					href="https://www.linkedin.com/in/igor-hamburger/"
+					title="LinkedIn"
 					target="_blank">
 					<Image
 						src={'/linkedin-icon.svg'}
@@ -39,12 +41,24 @@ export default function ContactMe() {
 				</Link>
 				<Link
 					href="https://github.com/igor-ht"
+					title="GitHub"
 					target="_blank">
 					<Image
 						src="https://cjyntkhqgtdnmlhfkwab.supabase.co/storage/v1/object/sign/skills/github.svg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJza2lsbHMvZ2l0aHViLnN2ZyIsImlhdCI6MTY4OTUwMTA2OSwiZXhwIjoxNzIxMDM3MDY5fQ.a3hgcDcnFKfJLg_1oS2DXZadLZZmBP-lOyI36c4Mpl0&t=2023-07-16T09%3A51%3A09.848Z"
 						height={48}
 						width={48}
 						alt="GitHub"
+					/>
+				</Link>
+				<Link
+					href="https://codepen.io/igor-ht/pens/popular"
+					title="CodePen"
+					target="_blank">
+					<Image
+						src="/codepen-icon.svg"
+						height={48}
+						width={48}
+						alt="CodePen"
 					/>
 				</Link>
 			</div>
